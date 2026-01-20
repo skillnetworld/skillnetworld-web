009b4d843736887b077f8786a88dbee537dcaac6
Ramjee194 <ramjeekumaryadav733@gmail.com>
2026-01-20 16:36:44 +0530

    Install react-icons and fix build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
+  images: { unoptimized: true },
+  turbopack: {
+    root: __dirname, // ensures correct root
   },
 };
 
